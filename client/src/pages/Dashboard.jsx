4f245950137bb21141023d23c1e0be0c54491f2c
unknown <akshaydeshmukh12345@gmail.com>
2026-08-04 23:48:28 +0530

    feat: connect dashboard to backend API

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,12 +1,62 @@
+import { useEffect, useState } from "react";
 import DashboardLayout from "../components/layout/DashboardLayout";
 import StatCard from "../components/common/StatCard";
+import api from "../services/api";
 
 function Dashboard() {
+  // ==========================
+  // State
+  // ==========================
+  const [dashboard, setDashboard] = useState({
+    totalEmployees: 0,
+  });
+
+  const [loading, setLoading] = useState(true);
+
+  // ==========================
+  // Fetch Dashboard Data
+  // ==========================
+  const fetchDashboardData = async () => {
+    try {
+      const response = await api.get("/dashboard");
+
+      console.log("Dashboard API:", response.data);
+
+      setDashboard(response.data.data);
+    } catch (error) {
+      console.error("Dashboard Error:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // ==========================
+  // Run Once
+  // ==========================
+  useEffect(() => {
+    fetchDashboardData();
+  }, []);
+
+  // ==========================
+  // Loading UI
+  // ==========================
+  if (loading) {
+    return (
+      <DashboardLayout>
+        <h2>Loading Dashboard...</h2>
+      </DashboardLayout>
+    );
+  }
+
+  // ==========================
+  // UI
+  // ==========================
   return (
     <DashboardLayout>
       <div className="dashboard-content">
         <div>
           <h1 className="page-title">Dashboard</h1>
+
           <p className="page-subtitle">
             Welcome to ShiftFlow Employee Management System
           </p>
@@ -15,7 +65,7 @@ function Dashboard() {
         <div className="stats-container">
           <StatCard
             title="Employees"
-            value="25"
+            value={dashboard.totalEmployees}
             icon="👨‍💼"
           />
 
